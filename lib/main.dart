c1932f7e0668908028cd0a17c0a6a9bd6570d408
AstrelSD <dsnelumdeniyage@students.nsbm.ac.lk>
2025-03-08 16:15:43 +0530

    Added splash and base menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile_app_roject/game/game_dev.dart';
+import 'package:mobile_app_roject/screens/platformer_splash.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  final PlatFormerGameDev platformer = PlatFormerGameDev();
-  runApp(GameWidget(game: platformer));
+  runApp( const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: PlatformerSplash(),
+  ));
 }
 
---
+++ lib/screens/platformer_main_menu.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PlatformerMainMenu extends StatelessWidget {
+  const PlatformerMainMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("Test File"),
+      ),
+    );
+  }
+}
