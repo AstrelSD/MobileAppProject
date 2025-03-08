499729ec617d4ee7a8e102fb8e6c025d15c29c3d
AstrelSD <dsnelumdeniyage@students.nsbm.ac.lk>
2025-03-08 15:53:28 +0530

    Updated main

---
+++ lib/game/game_dev.dart
@@ -0,0 +1,9 @@
+import 'dart:async';
+import 'package:flame/game.dart';
+
+class PlatFormerGameDev extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,10 @@
 import 'package:flame/game.dart';
-import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
-import 'package:flame/parallax.dart';
+import 'package:mobile_app_roject/game/game_dev.dart';
 
 void main() {
-  runApp(GameWidget(game: ProjectGame()));
+  WidgetsFlutterBinding.ensureInitialized();
+  final PlatFormerGameDev platformer = PlatFormerGameDev();
+  runApp(GameWidget(game: platformer));
 }
 
-class ProjectGame extends FlameGame {
-  @override
-  Future<void> onLoad() async {
-    await super.onLoad();
-
-    // Load the parallax background
-    final mountainBackground = await loadParallaxComponent(
-      [
-        ParallaxImageData('sea2.png'),
-      ],
-      baseVelocity: Vector2(10, 0), // Moves the background horizontally
-      velocityMultiplierDelta: Vector2(1.6, 1.0),
-    );
-
-    // Add the parallax background to the game
-    add(mountainBackground);
-  }
-}
