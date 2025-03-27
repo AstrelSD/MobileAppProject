0bc40427244a5e275ee145ed51cabf86092bf903
AstrelSD <dsnelumdeniyage@students.nsbm.ac.lk>
2025-03-27 12:49:30 +0530

    Created hud overlay initial

--- lib/game/game_dev.dart
+++ lib/game/game_dev.dart
@@ -3,24 +3,27 @@ import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:mobile_app_roject/levels/base_level.dart';
 import 'package:mobile_app_roject/levels/level_3.dart';
- 
-class PlatFormerGameDev extends FlameGame {
+import 'package:mobile_app_roject/screens/game_hud.dart';
 
+class PlatFormerGameDev extends FlameGame {
   late final CameraComponent cam;
   late final Level activeLevel;
-  
+  late GameHud hud;
+
   @override
   FutureOr<void> onLoad() async {
     await images.loadAllImages();
     activeLevel = Level3();
+    hud = GameHud();
     _loadGame(activeLevel);
     return super.onLoad();
   }
 
   void _loadGame(Level level) {
     level = activeLevel;
-    cam = CameraComponent.withFixedResolution(world: level, width: size.x, height: size.y);
+    cam = CameraComponent.withFixedResolution(
+    world: level, width: size.x, height: size.y);
     cam.viewfinder.anchor = Anchor.topLeft;
-    addAll([cam, activeLevel]);
+    addAll([cam, hud, activeLevel]);
   }
 }
