b7063632d194b17e90a0313767d2cd05975cbe6e
AstrelSD <dsnelumdeniyage@students.nsbm.ac.lk>
2025-03-29 16:11:10 +0530

    Updated Character select UI

---
+++ lib/widgets/menu_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MenuButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+  const MenuButton({ super.key, required this.onPressed, required this.text });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 250,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.greenAccent,
+          foregroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+            side: BorderSide(
+                color: const Color.fromARGB(255, 50, 50, 37), width: 4),
+          ),
+          elevation: 10,
+          shadowColor: Colors.black,
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            fontFamily: 'PixelFont',
+            shadows: [
+              Shadow(
+                blurRadius: 5,
+                color: Colors.black,
+                offset: Offset(2, 2),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
