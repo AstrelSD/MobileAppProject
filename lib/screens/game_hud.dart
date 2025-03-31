c9f7e845a65d5604eda21b8495828639c45cdc77
Thimira-Hansana <148532453+Thimira-Hansana@users.noreply.github.com>
2025-04-01 00:14:24 +0530

    Fixed character and map displaying errors.

--- lib/screens/game_hud.dart
+++ lib/screens/game_hud.dart
@@ -1,17 +1,23 @@
-import 'dart:async';
-
 import 'package:flame/components.dart';
+import 'package:flame/extensions.dart';
+import 'package:flame/text.dart';
+import 'package:flutter/material.dart';
 
 class GameHud extends PositionComponent {
   int health = 3;
+  
   @override
-  FutureOr<void> onLoad() {
-    late final TextComponent healthCounter;
-
-    healthCounter = TextComponent(
+  Future<void> onLoad() async {
+    final healthText = TextComponent(
       text: 'Health: $health',
-      position: Vector2(0, 30),
+      position: Vector2(20, 20),
+      textRenderer: TextPaint(
+        style: const TextStyle(
+          fontSize: 24,
+          color: Color.fromARGB(255, 235, 225, 225),
+        ),
+      ),
     );
-    add(healthCounter);
+    add(healthText);
   }
 }
--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 import 'package:mobile_app_roject/game/game_dev.dart';
-import 'package:mobile_app_roject/screens/settings/settings_overlay.dart';
 
 class GameScreen extends StatefulWidget {
   final String initialLevel;
@@ -10,7 +9,7 @@ class GameScreen extends StatefulWidget {
   const GameScreen({
     super.key,
     required this.initialLevel,
-    required this.character
+    required this.character,
   });
 
   @override
@@ -32,30 +31,8 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          GameWidget(
-            game: game,
-          ),
-          Positioned(
-            top: 40,
-            right: 20,
-            child: IconButton(
-              icon: const Icon(
-                Icons.settings,
-                size: 32,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => const SettingsOverlay(),
-                  barrierColor: Colors.black.withOpacity(0.5),
-                );
-              },
-            ),
-          ),
-        ],
+      body: GameWidget(
+        game: game,
       ),
     );
   }
