6e812357771ab92bee015998feb8627d22ada7a8
jjakshigan@students.nsbm.ac.lk <jjakshigan@students.nsbm.ac.lk>
2025-04-02 12:44:24 +0530

    Auto stash before merge of "Develop" and "origin/Develop"

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,18 @@
 import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-// import 'package:hive_flutter/hive_flutter.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:mobile_app_roject/screens/platformer_splash.dart';
+import 'package:mobile_app_roject/screens/sign_in_page.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   Flame.device.fullScreen();
   Flame.device.setLandscape();
-  // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
-
-  // SystemChrome.setPreferredOrientations([
-  //   DeviceOrientation.landscapeLeft,
-  //   DeviceOrientation.landscapeRight,
-  // ]);
 
   runApp(const MyApp());
 }
@@ -23,9 +22,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PlatformerSplash(),
+      home: AuthWrapper(), // Checks if user is signed in
+    );
+  }
+}
+
+// 🔹 Auth Wrapper: Checks user authentication state
+class AuthWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator()); // Loading state
+        } else if (snapshot.hasData) {
+          return const PlatformerSplash(); // User is signed in, go to game
+        } else {
+          return SignInPage(); // User is NOT signed in, go to sign-in
+        }
+      },
     );
   }
 }
