03abe886542de3b8e05ed5c9a32cdabb8adb1dcb
AnjaleeB02 <155352706+AnjaleeB02@users.noreply.github.com>
2025-04-02 12:15:19 +0530

    Add files via upload

---
+++ assets/tiles/Tiles_Beach.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Tiles_Beach" tilewidth="16" tileheight="16" tilecount="864" columns="24">
+ <image source="Tiles - Beach.png" width="384" height="576"/>
+</tileset>
