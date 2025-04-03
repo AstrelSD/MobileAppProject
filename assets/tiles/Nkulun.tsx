57a0c5cd28a30f7bb9d394314ae082c81e37528f
anusarawick <anusarawick@gmail.com>
2025-04-03 12:34:54 +0530

    Tileset updates

--- assets/tiles/Nkulun.tsx
+++ assets/tiles/Nkulun.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="Nkulun" tilewidth="16" tileheight="16" tilecount="128" columns="8">
- <image source="../../../../Nkulun.png" width="128" height="256"/>
+ <image source="../Terrain/Nkulun.png" width="128" height="256"/>
 </tileset>
