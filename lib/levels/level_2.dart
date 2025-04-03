a480665123f534844f1c65f98812478cca675f77
AnjaleeB02 <anjaleeb02@gmail.com>
2025-04-03 11:22:24 +0530

    updated level 2 map

--- lib/levels/level_2.dart
+++ lib/levels/level_2.dart
@@ -1,7 +1,7 @@
 import 'package:mobile_app_roject/levels/base_level.dart';
 
 class Level2 extends Level {
-  Level2({required super.character}) : super(activeLevel: "level2.tmx");
+  Level2({required super.character}) : super(activeLevel: "level2_map.tmx");
 
   @override
   Future<void> onLoad() async {
