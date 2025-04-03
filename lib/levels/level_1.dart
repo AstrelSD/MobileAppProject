7c811421290c2e115dddce1c71fc27c8d8c4674d
jjakshigan@students.nsbm.ac.lk <jjakshigan@students.nsbm.ac.lk>
2025-04-03 15:34:10 +0530

    WIP on Develop

--- lib/levels/level_1.dart
+++ lib/levels/level_1.dart
@@ -1,7 +1,7 @@
 import 'package:mobile_app_roject/levels/base_level.dart';
 
 class Level1 extends Level {
-  Level1({required super.character}) : super(activeLevel: "level3.tmx");
+  Level1({required super.character}) : super(activeLevel: "level2_map.tmx");
 
   @override
   Future<void> onLoad() async {
