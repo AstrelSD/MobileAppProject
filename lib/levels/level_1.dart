1a16728078396b5ca59be4720b02cb29ddb76c43
AnjaleeB02 <155352706+AnjaleeB02@users.noreply.github.com>
2025-04-03 20:00:17 +0530

    Update level_1.dart

--- lib/levels/level_1.dart
+++ lib/levels/level_1.dart
@@ -1,7 +1,7 @@
 import 'package:mobile_app_roject/levels/base_level.dart';
 
 class Level1 extends Level {
-  Level1({required super.character}) : super(activeLevel: "level3.tmx");
+  Level1({required super.character}) : super(activeLevel: "level1.tmx");
 
   @override
   Future<void> onLoad() async {
